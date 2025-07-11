3301b1398be27356df305fc4be9d1d0735a648c9
Ayush Saksena <137744036+ayushsaksena30@users.noreply.github.com>
2025-07-12 01:51:34 +0530

    Added: RightPanel Experience

---
+++ src/lib/components/RightPanel.svelte
@@ -0,0 +1,77 @@
+<div class="mt-32"></div>
+<div class="mt-4 mb-8 ml-8 text-left">
+  <h2 class="text-2xl font-bold">
+    About
+  </h2>
+
+  <p class="text-gray-700 text-md ml-14 mt-4">
+    I have built multiple projects in my free time. I have also worked on multiple projects in my college.
+  </p>
+</div>
+
+<div class="mt-8 mb-8 ml-8 text-left">
+  <h2 class="text-2xl font-bold">
+    Experience
+  </h2>
+
+  <h3 class="text-gray-700 text-md mt-8">
+    <div class="flex items-center gap-2">
+      <img src="/images/icons/bitcoin.svg" alt="Bitcoin" class="w-12 h-12" />
+      <a 
+      href="http://summerofbitcoin.org/2025-accepted-projects" 
+      target="_blank" 
+      rel="noopener noreferrer">
+        <span class="text-2xl font-bold underline">Summer of Bitcoin 2025</span>
+      </a>
+    </div>
+    <div class="flex flex-col items-start gap-2">
+      <span class="text-md font-bold ml-14">Software Engineer Intern @ Breez</span>
+      <p class="text-gray-700 text-md ml-14">
+        Selected as one of 68 contributors out of 45000+ applicants globally.
+      </p>
+    </div>
+
+  </h3>
+
+  <h3 class="text-gray-700 text-md mt-16">
+    <div class="flex items-center gap-2">
+      <img src="/images/icons/bsoc.png" alt="BSOC'25" class="w-12 h-12" />
+      <a 
+      href="https://github.com/bsoc-bitbyte/Nudj" 
+      target="_blank" 
+      rel="noopener noreferrer">
+        <span class="text-2xl font-bold underline">Bitbyte Summer of Code 2025</span>
+      </a>
+    </div>
+    <div class="flex flex-col items-start gap-2">
+      <span class="text-md font-bold ml-14">Android Maintainer @ Nudj</span>
+      <p class="text-gray-700 text-md ml-14">
+        Maintained and developed FOSS Android app simplifying college events.
+      </p>
+    </div>
+  </h3>
+</div>
+
+<div class="mt-8 mb-8 ml-8 text-left">
+  <h2 class="text-2xl font-bold">
+    Tech Stack
+  </h2>
+</div>
+
+<div class="mt-8 mb-8 ml-8 text-left">
+  <h2 class="text-2xl font-bold">
+    Projects
+  </h2>
+</div>
+
+<div class="mt-8 mb-8 ml-8 text-left">
+  <h2 class="text-2xl font-bold">
+    Resume
+  </h2>
+</div>
+
+<div class="mt-8 mb-8 ml-8 text-left">
+  <h2 class="text-2xl font-bold">
+    Resume
+  </h2>
+</div>
