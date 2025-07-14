b5f94bde3392078be37722791ae16b4da2846989
Ayush Saksena <ayushsaksena@MacBook-Air-5.local>
2025-07-15 03:53:29 +0530

    Added: Techstack, minor changes.

--- src/lib/components/RightPanel.svelte
+++ src/lib/components/RightPanel.svelte
@@ -9,7 +9,7 @@
   </h2>
 
   <div class="flex items-center gap-2 mt-4">
-    <img src="/images/icons/iiitdmj.jpg" alt="IIITDM Jabalpur" class="w-11 h-11 self-center" />
+    <img src="/images/icons/iiitdmj.jpg" alt="IIITDM Jabalpur" class="w-12 h-12 self-center" />
     <p class="text-gray-700 text-md">
       I am Pre-Final year student at Indian Institute of Information Technology, Jabalpur.
     </p>
@@ -46,7 +46,7 @@
         <TechStackItem imgPath="/images/lang/react.svg" name="React" />
         <TechStackItem imgPath="/images/lang/next-js.svg" name="Next.js" />
         <TechStackItem imgPath="/images/lang/tailwind.png" name="Tailwind CSS" />
-        <TechStackItem imgPath="/images/lang/git.svg" name="Git" />
+        <TechStackItem imgPath="/images/tools/git.svg" name="Git" />
       </div>
       <span class="text-md font-bold ml-14">Work-</span>
       <div class="text-gray-700 text-md ml-14">
@@ -83,8 +83,8 @@
       <div class="flex flex-wrap items-center gap-3 ml-14 mt-2">
         <TechStackItem imgPath="/images/lang/kotlin.png" name="Kotlin" />
         <TechStackItem imgPath="/images/lang/compose.png" name="Jetpack Compose" />
-        <TechStackItem imgPath="/images/lang/firebase.svg" name="Firebase" />
-        <TechStackItem imgPath="/images/lang/github.svg" name="Github" />
+        <TechStackItem imgPath="/images/tools/firebase.svg" name="Firebase" />
+        <TechStackItem imgPath="/images/tools/github.svg" name="Github" />
       </div>
       <span class="text-md font-bold ml-14">Work-</span>
       <div class="text-gray-700 text-md ml-14">
@@ -102,6 +102,55 @@
   <h2 class="text-2xl font-bold">
     Tech Stack
   </h2>
+  <h3 class="text-md font-bold ml-14 mt-8">App Development</h3>
+  <div class="flex flex-wrap items-center gap-3 ml-14 mt-2">
+    <TechStackItem imgPath="/images/lang/kotlin.png" name="Kotlin" />
+    <TechStackItem imgPath="/images/lang/java.svg" name="Java" />
+    <TechStackItem imgPath="/images/lang/flutter.svg" name="Flutter" />
+    <TechStackItem imgPath="/images/lang/compose.png" name="Jetpack Compose" />
+    <TechStackItem imgPath="/images/lang/xml.svg" name="XML" />
+    <TechStackItem imgPath="/images/tools/firebase.svg" name="Firebase" />
+  </div>
+  <h3 class="text-md font-bold ml-14 mt-8">Blockchain Development</h3>
+  <div class="flex flex-wrap items-center gap-3 ml-14 mt-2">
+    <TechStackItem imgPath="/images/lang/rust.svg" name="Rust" />
+    <TechStackItem imgPath="/images/lang/tokio.png" name="Tokio" />
+    <TechStackItem imgPath="/images/lang/js.png" name="JavaScript" />
+    <TechStackItem imgPath="/images/lang/wasm.svg" name="Web Assembly" />
+  </div>
+  <h3 class="text-md font-bold ml-14 mt-8">AI/ML</h3>
+  <div class="flex flex-wrap items-center gap-3 ml-14 mt-2">
+    <TechStackItem imgPath="/images/lang/python.svg" name="Python" />
+    <TechStackItem imgPath="/images/lang/pandas.svg" name="Pandas" />
+    <TechStackItem imgPath="/images/lang/numpy.svg" name="Numpy" />
+    <TechStackItem imgPath="/images/lang/scikit-learn.png" name="Scikit-Learn" />
+    <TechStackItem imgPath="/images/lang/tensorflow.svg" name="TensorFlow" />
+    <TechStackItem imgPath="/images/lang/xgb.png" name="XG Boost" />
+    <TechStackItem imgPath="/images/lang/opencv.png" name="OpenCV" />
+    <TechStackItem imgPath="/images/lang/seaborn.png" name="Seaborn" />
+    <TechStackItem imgPath="/images/lang/matplotlib.png" name="Matplotlib" />
+
+  </div>
+  <h3 class="text-md font-bold ml-14 mt-8">Web Development</h3>
+  <div class="flex flex-wrap items-center gap-3 ml-14 mt-2">
+    <TechStackItem imgPath="/images/lang/react.svg" name="React" />
+    <TechStackItem imgPath="/images/lang/svelte.svg" name="Svelte" />
+    <TechStackItem imgPath="/images/lang/next-js.svg" name="Next.js" />
+    <TechStackItem imgPath="/images/lang/tailwind.png" name="Tailwind CSS" />
+    <TechStackItem imgPath="/images/lang/vite.svg" name="Vite" />
+    <TechStackItem imgPath="/images/lang/turbopack.png" name="Turbopack" />
+    <TechStackItem imgPath="/images/lang/html.svg" name="HTML" />
+    <TechStackItem imgPath="/images/lang/css.svg" name="CSS" />
+  </div>
+  <h3 class="text-md font-bold ml-14 mt-8">Development Tools</h3>
+  <div class="flex flex-wrap items-center gap-3 ml-14 mt-2">
+    <TechStackItem imgPath="/images/tools/vscode.svg" name="VS Code" />
+    <TechStackItem imgPath="/images/tools/studio.png" name="Android Studio" />
+    <TechStackItem imgPath="/images/tools/cursor.png" name="Cursor" />
+    <TechStackItem imgPath="/images/tools/git.svg" name="Git" />
+    <TechStackItem imgPath="/images/tools/github.svg" name="Github" />
+    <TechStackItem imgPath="/images/tools/figma.svg" name="Figma" />
+  </div>
 </div>
 
 <div class="mt-8 mb-8 ml-8 text-left">
